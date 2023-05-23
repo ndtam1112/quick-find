ec9a7e6e0b95e38dd58dbfe51b68c4038e4701e7
ndtam1112 <ndtam1010@gmail.com>
2023-05-23 12:29:56 +0700

    .

--- src/index.js
+++ src/index.js
@@ -1,73 +1,67 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import App from "./App";
-import ErrorPage from "./ErrorPage";
-import "./index.css";
-import ViewCoupon from "./Presentation/View/Coupon/ViewCoupon";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import ErrorPage from './ErrorPage'
+import './index.css'
+import ViewCoupon from './Presentation/View/Coupon/ViewCoupon'
 import ViewHospital from './Presentation/View/Hospital/ViewHospital'
-import ViewManagerDriver from "./Presentation/View/ManagerDriver/ViewManagerDriver";
-import ViewServiceCenter from "./Presentation/View/ServiceCenter/ViewServiceCenter";
-import ViewManagerService from "./Presentation/View/ManagerService/ViewManagerService";
-import ViewManagerOrder from "./Presentation/View/ManagerOrder/ViewManagerOrder";
-import ViewHome from "./Presentation/View/Home/ViewHome.jsx";
+import ViewManagerDriver from './Presentation/View/ManagerDriver/ViewManagerDriver'
+import ViewServiceCenter from './Presentation/View/ServiceCenter/ViewServiceCenter'
+import ViewManagerService from './Presentation/View/ManagerService/ViewManagerService'
+import ViewManagerOrder from './Presentation/View/ManagerOrder/ViewManagerOrder'
+import ViewHome from './Presentation/View/Home/ViewHome.jsx'
 import Login from './Presentation/View/Login'
-import SignUp from "./Presentation/View/SignUp";
+import SignUp from './Presentation/View/SignUp'
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <ViewHome/>,
+    path: '/',
+    element: <ViewHome />,
     errorElement: <ErrorPage />,
   },
   {
-    path: "/quan-ly-benh-vien",
+    path: '/quan-ly-benh-vien',
     element: <ViewHospital />,
   },
   {
-    path: "/quan-ly-trung-tam",
+    path: '/quan-ly-trung-tam',
     element: <ViewServiceCenter />,
   },
   {
-    path: "/quan-ly-tai-xe",
+    path: '/quan-ly-tai-xe',
     element: <ViewManagerDriver />,
   },
   {
-    path: "/quan-ly-phieu-giam-gia",
+    path: '/quan-ly-phieu-giam-gia',
     element: <ViewCoupon />,
   },
   {
-    path: "/quan-ly-dich-vu",
+    path: '/quan-ly-dich-vu',
     element: <ViewManagerService />,
   },
   {
-    path: "/quan-ly-tai-xe",
+    path: '/quan-ly-tai-xe',
     element: <ViewManagerDriver />,
   },
   {
-    path: "/quan-ly-chuyen-xe",
+    path: '/quan-ly-chuyen-xe',
     element: <ViewManagerOrder />,
   },
   {
-    path: "/dang-nhap",
+    path: '/dang-nhap',
     element: <Login />,
   },
   {
-    path: "/dang-ky",
+    path: '/dang-ky',
     element: <SignUp />,
   },
-]);
+])
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
-);
-
-
+)
 
 // import React from 'react';
 // import ReactDOM from 'react-dom/client'
